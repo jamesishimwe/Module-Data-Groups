6bc8f58744cc58d1cb373f305ee4601b3f9cda98
jamesishimwe <james123ishimwe@gmail.com>
2026-04-07 19:10:39 +0200

    Add test for duplicate arrays

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -35,5 +35,9 @@ describe("dedupe", () => {
         expect(dedupe([1, 2, 3])).toEqual([1, 2, 3]);
     });
 
-   
+    it("given an array with strings or numbers, it removes the duplicate values, preserving the first occurrence of each element", () => {
+        expect(dedupe(['a','a','a','b','b','c'])).toEqual(['a','b','c']);
+        expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toEqual([5, 1, 2, 3, 8]);
+        expect(dedupe([1, 2, 1])).toEqual([1, 2]);
+    });
 });
