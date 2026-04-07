c9001bc7fd461652049d70c9b6f33809d5cec6b4
jamesishimwe <james123ishimwe@gmail.com>
2026-04-07 19:07:09 +0200

    Implement a for loop for adding to the array unique elements

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,4 +1,9 @@
 function dedupe(list) {
     let listWithoutDuplicates = [];
-    
+    for (let i = 0; i < list.length; i++) {
+        if(listWithoutDuplicates.includes(list[i]) === false) {
+            listWithoutDuplicates.push(list[i]);
+        }
+    }
+    return listWithoutDuplicates;
 }
