0f551a381c114377e3800cc703466b9fb601838b
jamesishimwe <james123ishimwe@gmail.com>
2026-04-07 19:28:22 +0200

    Implement findMax

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
 function findMax(elements) {
+let max = -Infinity;
+for (let i = 0; i < elements.length; i++) {
+  if (typeof elements[i] === "number" && elements[i] > max) {
+    max = elements[i];
+  }
+}
+return max;
 }
 
 module.exports = findMax;
