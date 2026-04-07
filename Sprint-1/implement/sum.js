796aa7ae67de5c1b14cf64feb718ad8fde8aa747
jamesishimwe <james123ishimwe@gmail.com>
2026-04-07 19:44:34 +0200

    Implement sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+    let total = 0;
+    for (let element of elements) {
+        if (typeof element === 'number') {
+            total += element;
+        }
+    }
+    return total;
 }
 
 module.exports = sum;
