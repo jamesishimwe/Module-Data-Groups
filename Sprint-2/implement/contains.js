b05607426dfe1708567425b1a3cfe6f4f67d98e9
jamesishimwe <james123ishimwe@gmail.com>
2026-04-10 20:29:08 +0200

    Implement the function contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,6 @@
-function contains() {}
+function contains(Obj,key) {
+    if(Obj === null || typeof Obj !== "object" || Array.isArray(Obj)) return false;
+     return Object.prototype.hasOwnProperty.call(Obj, key);
+}
 
 module.exports = contains;
