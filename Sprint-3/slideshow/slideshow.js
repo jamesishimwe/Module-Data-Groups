eff3edc67c8e56e16ddfaa2ee7d838a02a695ada
jamesishimwe <james123ishimwe@gmail.com>
2026-05-01 17:40:39 +0200

    Implement slideshow buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,5 +4,28 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
+let currentIndex = 0;
+let intervalId = null;
 
-// Write your code here
+const imgElement = document.querySelector("#carousel-img");
+const forwardBtn = document.querySelector("#forward-btn");
+const backwardBtn = document.querySelector("#backward-btn");
+const autoForwardBtn = document.querySelector("#auto-forward");
+const autoBackBtn = document.querySelector("#auto-backward");
+const stopBtn = document.querySelector("#stop");
+
+function updateImage() {
+    imgElement.src = images[currentIndex];
+}
+function moveForward() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function moveBackward() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+}
+
+forwardBtn.addEventListener("click", moveForward);
+backwardBtn.addEventListener("click", moveBackward);
